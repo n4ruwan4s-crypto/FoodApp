a1e6a7099b1b244e49333f819846cddb1948e182
Ruwan Sampath <n4ruwan4s@gmail.com>
2026-09-06 13:51:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLtGfZnn34ecEz9bBgc5tawwUTPMLukHo",
+  authDomain: "kussiya-lunch.firebaseapp.com",
+  projectId: "kussiya-lunch",
+  storageBucket: "kussiya-lunch.firebasestorage.app",
+  messagingSenderId: "1032681220499",
+  appId: "1:1032681220499:web:90d7eb2f3ef21df576e066"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
